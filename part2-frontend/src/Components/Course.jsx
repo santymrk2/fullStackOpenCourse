a6a0bdc91bda666f3ff3ea69f45366f0df49718c
santymrk2 <santymc@icloud.com>
2024-07-04 23:05:23 -0300

    part2 exercise 2 complete

--- part2-frontend/src/Components/Course.jsx
+++ part2-frontend/src/Components/Course.jsx
@@ -1,6 +1,6 @@
 import Header from "./Header"
 import Content from "./Content"
-//import Total from "./Total"
+import Total from "./Total"
 
 
 const Course = ({course}) => {
@@ -8,7 +8,7 @@ const Course = ({course}) => {
     <>
       <Header name={course.name}/>
       <Content parts={course.parts}/>
-      {/*<Total parts={course.parts} />*/}
+      <Total parts={course.parts} />
     </>
     
   )
